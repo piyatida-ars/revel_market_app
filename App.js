1b562775dc428274ecbc0684b842f222421b1b8d
piyatida-ars <pongpiyatida@gmail.com>
2020-01-25 09:58:51 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Login } from './View/Login/Login'
+import { Home } from './View/Home/Home'
+
+const App = () => {
+  return (
+    <Home/>
+  );
+};
+
+export default App;
---
+++ GOBALS.js
@@ -0,0 +1,3 @@
+export default {
+	URL: 'http://191.1.2.225:5001/',
+}
